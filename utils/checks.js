1a8c0c3c01d07e965f366233393f87cc268da796
Saurabh Kumar Singhal <saurabh.singhal@agreeya.net>
2026-03-05 15:57:18 +0530

    Updated check.js - modified thresholds / validation logic

--- utils/checks.js
+++ utils/checks.js
@@ -2,7 +2,7 @@ import { check } from 'k6';
 
 export function validateResponse(response) {
   return check(response, {
-    'status is 200': (r) => r.status === 200,
+    'status is 200': (r) => r.status === 400,
     'response time < 1000ms': (r) => r.timings.duration < 1000,
   });
 }
